81b1aa955f4af425d1703367937dc400d99d4fba
nogando <nathanielogando@gmail.com>
2025-10-09 21:54:22 -0400

    create skeleton of menu start of home menu

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -3,7 +3,24 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
+        //Create a loop that will keep going for ever as long as i exit it
+
+        //Home Menu
+        homeMenu();
 
 
+
+
+    }
+
+    public static void homeMenu(){
+        String homeMenu = """
+                    ========= Product Menu =========
+                        D- Add Deposit
+                        P- Make Payment(Debit)
+                        L- Ledger
+                        X- Exit
+                    """;
+                System.out.println(homeMenu);
     }
 }
