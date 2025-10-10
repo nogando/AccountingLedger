5599d96849edb71ab3f203427ef688e343f92f1f
nogando <nathanielogando@gmail.com>
2025-10-09 22:09:08 -0400

    basic menu working

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,12 +1,43 @@
 package com.pluralsight;
 
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //Create a loop that will keep going for ever as long as i exit it
 
-        //Home Menu
-        homeMenu();
+        boolean running = true;
+        while(running == true){
+            //create scanner to get input for if else statement to make interactive menu
+            Scanner scanner = new Scanner(System.in);
+
+            String choice;
+            //create simple Home Menu with if else statement
+            homeMenu();
+            choice = scanner.nextLine();
+
+
+
+            if(choice.trim().equalsIgnoreCase("d")){
+                System.out.println("Adding Deposit");
+            }
+            else if(choice.trim().equalsIgnoreCase("p")){
+                System.out.println("Making Payment");
+            }
+            else if(choice.trim().equalsIgnoreCase("l")){
+                System.out.println("Showing Ledger");
+
+            } else if (choice.trim().equalsIgnoreCase("x")) {
+                running = false;
+                System.out.println("Goodbye!");
+
+            }
+            else{
+                System.out.println("Invalid Entry Selected");
+            }
+        }
+
 
 
 
@@ -15,7 +46,7 @@ public class Main {
 
     public static void homeMenu(){
         String homeMenu = """
-                    ========= Product Menu =========
+                    \n========= Product Menu =========
                         D- Add Deposit
                         P- Make Payment(Debit)
                         L- Ledger
