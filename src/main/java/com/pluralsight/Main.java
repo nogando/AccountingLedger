e7fd9e869d15d431e321cba25267498fb7d4d2e8
nogando <nathanielogando@gmail.com>
2025-10-09 22:37:36 -0400

    menu: list and ToDo's

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -5,28 +5,53 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Create a loop that will keep going for ever as long as i exit it
 
+
+
+        homeMenu();// Placed my home menu Outside of loop so that way it prints once
+        //after the loop is running i can always call my home menu method when i need it
+
+        //Create a loop that will keep going for ever as long as i exit it
         boolean running = true;
+
         while(running == true){
             //create scanner to get input for if else statement to make interactive menu
             Scanner scanner = new Scanner(System.in);
 
             String choice;
             //create simple Home Menu with if else statement
-            homeMenu();
+
             choice = scanner.nextLine();
 
 
 
             if(choice.trim().equalsIgnoreCase("d")){
                 System.out.println("Adding Deposit");
+                //todo prompt user for the deposit information and save it to the csv file
             }
             else if(choice.trim().equalsIgnoreCase("p")){
                 System.out.println("Making Payment");
+                //todo prompt user for the debit information and save it to the csv file
             }
             else if(choice.trim().equalsIgnoreCase("l")){
                 System.out.println("Showing Ledger");
+                //todo display the ledger screen
+
+                //create a sub screen that shows
+
+                //o A) All - Display all entries
+                //o D) Deposits - Display only the entries that are deposits into the account
+                //o P) Payments - Display only the negative entries (or payments)
+                //o R) Reports - A new screen that allows the user to run pre-defined reports or to run a custom search
+
+                    //create a sub screen that shows
+
+                    // 1) Month To Date
+                    // 2) Previous Month
+                    // 3) Year To Date
+                    // 4) Previous Year
+                    // 5) Search by Vendor - prompt the user for the vendor name and display all entries for that vendor
+                    // 0) Back - go back to the Ledger page
 
             } else if (choice.trim().equalsIgnoreCase("x")) {
                 running = false;
